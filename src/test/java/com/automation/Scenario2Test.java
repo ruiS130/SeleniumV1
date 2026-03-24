cb5e9e270e2e7663e6f810c2ec26767bd37927cf
Mingrui <yuan.ming@northeastern.edu>
2026-03-24 17:23:10 -0400

    Update Scene 2 to contain basic redirection

--- src/test/java/com/automation/Scenario2Test.java
+++ src/test/java/com/automation/Scenario2Test.java
@@ -1,4 +1,20 @@
 package com.automation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
 public class Scenario2Test {
+    WebDriver driver;
+    WebDriverWait wait;
+    Map<String, String> testData;
+    static final String SCENARIO_NAME = "Scenario1_Download_Transcript";
+
+    @Test
+    public void eventCreation() throws Exception {
+        driver.get("https://canvas.northeastern.edu");
+    }
+
 }
